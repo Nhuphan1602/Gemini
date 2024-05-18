9c8ab8fe25f1745102ed988c26d35865253be87c
phantamnhu <nhuphan160200@gmail.com>
2024-05-18 18:28:56 +0700

    project initialize

---
+++ Gemini/Gemini/GeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiApp.swift
+//  Gemini
+//
+//  Created by Phan Tam Nhu on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
